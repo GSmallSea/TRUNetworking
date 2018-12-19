2865600e9e83b2b99a936377ada9c017a18fc523
GSmallSea <gxiao_hai@163.com>
2018-12-19 14:38:06 +0800

    Initial commit

---
+++ Example/TRUNetworking/XHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XHAppDelegate.h
+//  TRUNetworking
+//
+//  Created by gxiao_hai@163.com on 12/19/2018.
+//  Copyright (c) 2018 gxiao_hai@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TRUNetworking/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TRUNetworking
+//
+//  Created by gxiao_hai@163.com on 12/19/2018.
+//  Copyright (c) 2018 gxiao_hai@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "XHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XHAppDelegate class]));
+    }
+}
